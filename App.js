7ae3a993836f5c721c85de13224ab2ed7abe221f
Alexander Summers <alexandersummers@Alexanders-MacBook-Air.local>
2020-02-23 21:05:43 -0500

    Lays down first version of specification for reviews, tests rendering a FlatList of reviews

--- App.js
+++ App.js
@@ -1,19 +1,65 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
+import { StyleSheet, Text, View, FlatList} from 'react-native';
+
+
+/*
+Reviews:
+Title
+Comment
+Rating
+
+VoteTotal (upvotes - downvotes)
+Replies (list of Text)
+*/
+
+
+const testReviews = [
+  {title: "My First Review",
+  comment: "This is a test review, this company is my favorite.",
+  rating: 4,
+  voteTotal: 0,
+  Replies: []
+  }
+]
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>MyCompany Reviews</Text>
+
+      
+      <FlatList
+      data={testReviews}
+      renderItem={({item}) => <Review {...item}/>}
+      />
+
     </View>
   );
 }
+
+class Review extends Component {
+
+  render() {
+    return(
+      <View>
+        <Text>{this.props.title}</Text>
+        <Text>{this.props.comment}</Text>
+
+        <Text>{this.props.rating}</Text>
+
+        <Text>{this.props.voteTotal}</Text>
+      </View>
+    )
+  }
+
+}
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    padding: 20
   },
 });
