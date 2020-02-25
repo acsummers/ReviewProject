eda61a6b2e16353a25365f641c1c19ed86a6cac7
Alexander Summers <alexandersummers@Alexanders-MacBook-Air.local>
2020-02-24 23:17:11 -0500

    Finalizes functionality for review app

--- App.js
+++ App.js
@@ -38,8 +38,32 @@ class AppHome extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      addReviewsVisible: false
+      addReviewsVisible: false,
+      reviews: testReviews
+    }
 
+    this.voteIncrementFactory = (review) => {
+      return (increment) => {
+        review.voteTotal = review.voteTotal + increment
+        this.setState({reviews: this.state.reviews})
+      }
+    }
+  
+    this.submitReplyFactory = (review) => { 
+
+      return (reply) => {
+        if (reply.length > 0) {
+          review.replies.push(reply)
+          this.setState({reviews: this.state.reviews})
+        }
+
+      }
+    }
+
+    this.submitReview = (title, comment, rating) => {
+      var tempReviews = this.state.reviews
+      tempReviews.push({title: title, comment: comment, rating: rating, voteTotal: 0, replies: []})
+      this.setState({reviews: tempReviews})
     }
   }
 
@@ -50,14 +74,15 @@ class AppHome extends Component {
        <AddReviewModal 
         closeModal={() => this.setState({addReviewsVisible: false})} 
         modalVisible={this.state.addReviewsVisible}
-        submitReview={(title, comment, rating) => testReviews.push({title: title, comment: comment, rating: rating, voteTotal: 0, replies: []})}/>
+        submitReview={this.submitReview}/>
 
        <Text style={styles.homeTitle} >MyCompany Reviews</Text>
   
       
        <FlatList
-       data={testReviews}
-       renderItem={({item}) => <Review {...item}/>}
+       data={this.state.reviews}
+       extraData={this.state.reviews.map(x => x.voteTotal)}
+       renderItem={({item}) => <Review submitReply={this.submitReplyFactory(item)} incrementVote={this.voteIncrementFactory(item)} {...item}/>}
        />
 
       
@@ -77,12 +102,15 @@ class AppHome extends Component {
 
 class Review extends Component {
   constructor(props) {
+    super(props)
 
     this.state = {
       upvoted: false,
-      downvoted: false
+      downvoted: false,
+      addingReply: false,
+      currentReplyText: "",
+      repliesVisible: false
     }
-    super(props)
 
     renderStars = (rating) => {
       var toReturn = []
@@ -99,25 +127,41 @@ class Review extends Component {
     }
 
     upvoteClicked = () => {
+
+      if (this.state.downvoted == true) {
+          this.props.incrementVote(1)
+        }
+
       if (this.state.upvoted == false) { 
         this.setState({downvoted: false, upvoted: true})
-        this.props.setVote(1)
+        this.props.incrementVote(1)
+        
       }
       else {
         this.setState({downvoted: false, upvoted: false})
-        this.props.setVote(0)
+        this.props.incrementVote(-1)
       }
     }
 
     downvoteClicked = () => {
+
+      if (this.state.upvoted == true) {
+        this.props.incrementVote(-1)
+      }
+
       if (this.state.downvoted == false) { 
         this.setState({downvoted: true, upvoted: false})
-        this.props.setVote(-1)
+        this.props.incrementVote(-1)
       }
       else {
         this.setState({downvoted: false, upvoted: false})
-        this.props.setVote(0)
+        this.props.incrementVote(1)
       }
+    }
+
+    this.submitReply = () => {
+      this.props.submitReply(this.state.currentReplyText)
+      this.setState({addingReply: false, currentReplyText: ""})
     }
 
 
@@ -150,6 +194,20 @@ class Review extends Component {
           <TouchableOpacity onPress={downvoteClicked}>
             <MaterialCommunityIcons name={this.state.downvoted === true ? "arrow-down-bold" : "arrow-down-bold-outline"} size={32} color={"red"}/>
           </TouchableOpacity>
+        </View>
+
+
+        <View>
+
+          <View style={{flexDirection: 'row'}}>
+            <TouchableOpacity onPress={() => this.setState({repliesVisible: !this.state.repliesVisible})} style={{flex: 1, flexDirection: 'row', alignItems: 'center'}}><Text>{!this.state.repliesVisible ? "See" : "Hide"} {String(this.props.replies.length)} Replies</Text><MaterialIcons name={"add"} size={16}/></TouchableOpacity>
+            <TouchableOpacity onPress={() => this.setState({addingReply: !this.state.addingReply})} style={{flex: 1}}><Text>{!this.state.addingReply ? "Add Reply" : "Hide adding reply"}</Text></TouchableOpacity>
+          </View>
+
+          {this.state.addingReply ? <TextInput onChangeText={(text) => this.setState({currentReplyText: text})} onEndEditing={this.submitReply} style={styles.formInput}/> : <View/>}
+
+          {this.state.repliesVisible ? this.props.replies.map(reply => <Text>{reply}</Text> ) : <View/>}
+
         </View>
 
 
